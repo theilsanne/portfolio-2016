e6ff02810908d0f0d6a63b0c018d597096a1398b
Ethelia Lung <ethelialung1234@gmail.com>
2020-05-22 13:44:11 -0700

    First upload of 20161224 backup
    Doesn't include the assets/media folder

---
+++ rfid.php
@@ -0,0 +1,135 @@
+<?php include("header.php"); ?>
+                
+                <div class="box single hide" style="height: 200px;">
+                    <!-- navigation goes here -->
+                </div>
+            
+                <div class="box mainContent">
+                    <h1>RFID Portal</h1>
+                    
+                    <div class="box third">
+                        <h2>Client</h2>
+                        LX1 Technology Ltd.
+                        
+                        <br /><br />
+                        
+                        <h2>Role</h2>
+                        Visual Design
+                        
+                        <br /><br />
+                        
+                        <h2>Duration</h2>
+                        2 weeks / winter 2014
+                    </div>
+                    
+                    <div class="box twoThirds">
+                        <h2>Brief</h2>
+                        <blockquote>An RFID-enabled workflow is a system is an essential tool in an era that craves efficiency.</blockquote>
+                        
+                        <p>
+                            LX1 Technology Ltd. created a web application for an RFID system that includes an <strong>administrative portal</strong> and a messenger <strong>scanner</strong> interface. This project is the visual design of this portal.
+                        </p>
+                        
+                        <a href="assets/media/rfid/RFID_macbook_mockup.png" data-lightbox="privileged" data-title="RFID Mockup"><img src="assets/media/rfid/RFID_macbook_mockup.png" alt="RFID Mockup" /></a>
+                    </div>
+                    
+                    <hr />
+                    
+                    <div class="box single">
+                        <h2>Understanding</h2>
+                        
+                        <p>
+                            Both the administrators and messengers using this application are office workers, thus a need for a simple interface and intuitive design. 
+                        </p>
+                    </div>
+                    
+                    <div class="box single">
+                        <h2>UX Breakdown</h2>
+                        
+                        <blockquote>
+                            The three main considerations of the interface are the tracking of actions, the settings for undertaking actions, and the process of scanning.
+                        </blockquote>
+                        
+                        <p>
+                            When actions are taken, the associated data is tracked and visualised in simple tables. In some tables, the status of outgoing information is also taken into account.
+                        </p>
+                        
+                        <p>
+                            Previously, the settings screen was blocked out into floating sections that were difficult to track. The new design attempts to break each setting down into categories, and presents them in a linear fashion. 
+                        </p>
+                    </div>
+                    
+                    <hr />
+                    
+                    <div class="box single">
+                        <h2>Portal Design</h2>
+                        
+                        <div class="box half">
+                            <p>
+                                The portal is the heft of the project, in that it had to be a useable experience for an administrator who would have to sift through many pages. I chose to go for a very simple dashboard look and feel, and have a slide-in menu to allow for an immersive experience, as well as allow for more space for the main content.
+                            </p>
+                        </div>
+                        
+                        <div class="box half">
+                            <a href="assets/media/rfid/rfid-landing.png" data-lightbox="privileged" data-title="RFID Landing Page"><img src="assets/media/rfid/rfid-landing.png" alt="RFID Landing Page" /></a>
+                        </div>
+                        
+                        <div class="box half">
+                            <a href="assets/media/rfid/rfid-tasks.png" data-lightbox="privileged" data-title="RFID Tasks Page"><img src="assets/media/rfid/rfid-tasks.png" alt="RFID Tasks Page" /></a>
+                        </div>
+                        
+                        <div class="box half">
+                            <a href="assets/media/rfid/rfid-sms_view.png" data-lightbox="privileged" data-title="RFID SMS View"><img src="assets/media/rfid/rfid-sms_view.png" alt="RFID SMS View" /></a>
+                        </div>
+                        
+                        <div class="box half">
+                            <p>
+                                In addition to the tables, I had to include functionality that allowed the user to change the settings. Rethinking the structure of the form/input elements was a challenge, in that I was hoping to conserve space as well as make the interface more intuitive than it was previously.  
+                            </p>
+                        </div>
+                        
+                        <div class="box half">
+                            <a href="assets/media/rfid/rfid-settings.png" data-lightbox="privileged" data-title="RFID Transaction View"><img src="assets/media/rfid/rfid-settings.png" alt="RFID Transaction View" /></a>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="box single">
+                        <h2>Scanner Design</h2>
+                        
+                        <div class="box half">
+                            <p>
+                                The scanner was the second portion of the project. The main function of this was to have an interface that was simple, and easy for a 'messenger' to understand. The user flow through this would be to start the scanner, scan the files, and send the records off. Previously, the interface had included many input form fields that were disabled, and more information than necessary for users.  
+                            </p>
+                            
+                            <p>
+                                My solution, then was to only include what was necessary, in a manner that did not suggest that the information was contained within input fields, and had a clear way of expressing the controls that the user has access to.
+                            </p>
+                        </div>
+                        
+                        <div class="box half">
+                            <a href="assets/media/rfid/rfid-scanner.png" data-lightbox="privileged" data-title="RFID Scanner"><img src="assets/media/rfid/rfid-scanner.png" alt="RFID Scanner" /></a>
+                        </div>
+                    </div>
+                    
+                    <hr />
+                    
+                    <div class="box single">
+                        <h2>Fin</h2>
+                        
+                        <p>
+                            The RFID Portal was a great exercise in simplifying the complex settings and capabilities for both administrators and users. Although in hindsight, the UI solution is not ideal, this was a very good exercise in understanding the audience and how to create an easy experience for them.  
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="box lowerPag">
+                    <a href="nest.php" class="left-button">
+                        <h1>Nest Scheduler</h1>
+                    </a>
+                    <a href="privileged.php" class="right-button">
+                        <h1>Privileged Only</h1>
+                    </a>
+                </div>
+
+<?php include("footer.php"); ?>
